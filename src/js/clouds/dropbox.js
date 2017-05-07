b0eb213c314930855f47eb2fbc7b900a4241e6c2
metal03326 <metallica.uktc@gmail.com>
2017-05-07 11:34:31 +0300

    Replaced var with let and removed this = self

--- src/js/clouds/dropbox.js
+++ src/js/clouds/dropbox.js
@@ -2,32 +2,52 @@
  * Noisy communication module for Dropbox
  *
  * @author metal03326
- * @version 20170506
+ * @version 20170507
  */
 
 // Dropbox singleton to be passed to Noisy
-var dropbox = new Cloud( {
+let dropbox = new Cloud( {
 	name: 'Dropbox',
 
 	urls: {
-		connect: "//www.dropbox.com/oauth2/authorize?response_type=token&client_id=" +
-							encodeURIComponent( "5lq1huyyozezyl5" ) +
-							"&redirect_uri=" +
-							encodeURIComponent( location.protocol + '//' + location.host + location.pathname ),
-		info: {url: 'https://api.dropboxapi.com/2/users/get_current_account', method: 'POST'},
-		query: {url: 'https://api.dropboxapi.com/2/files/list_folder', method: 'POST', jsonBody: '{"path":"{{path}}"}', headers: {'Content-Type': 'application/json; charset=utf-8'}},
-		loadPlaylist: {url:'https://content.dropboxapi.com/2/files/download', method: 'POST', headers: {'Content-Type': ' ', 'Dropbox-API-Arg': '{"path": "{{path}}"}'}},
-		play: {url: 'https://api.dropboxapi.com/2/files/get_temporary_link', method: 'POST', jsonBody: '{"path":"{{path}}"}', headers: {'Content-Type': 'application/json; charset=utf-8'}},
-		savePlaylist: {url: 'https://content.dropboxapi.com/2/files/upload', method: 'POST', headers: {'Content-Type': 'application/octet-stream', 'Dropbox-API-Arg': '{"path": "{{path}}","mode": "add","autorename": true,"mute": false}'}}
+		connect     : '//www.dropbox.com/oauth2/authorize?response_type=token&client_id=' +
+		encodeURIComponent( '5lq1huyyozezyl5' ) +
+		'&redirect_uri=' +
+		encodeURIComponent( location.protocol + '//' + location.host + location.pathname ),
+		info        : { url: 'https://api.dropboxapi.com/2/users/get_current_account', method: 'POST' },
+		query       : {
+			url     : 'https://api.dropboxapi.com/2/files/list_folder',
+			method  : 'POST',
+			jsonBody: '{"path":"{{path}}"}',
+			headers : { 'Content-Type': 'application/json; charset=utf-8' }
+		},
+		loadPlaylist: {
+			url    : 'https://content.dropboxapi.com/2/files/download',
+			method : 'POST',
+			headers: { 'Content-Type': ' ', 'Dropbox-API-Arg': '{"path": "{{path}}"}' }
+		},
+		play        : {
+			url     : 'https://api.dropboxapi.com/2/files/get_temporary_link',
+			method  : 'POST',
+			jsonBody: '{"path":"{{path}}"}',
+			headers : { 'Content-Type': 'application/json; charset=utf-8' }
+		},
+		savePlaylist: {
+			url    : 'https://content.dropboxapi.com/2/files/upload',
+			method : 'POST',
+			headers: {
+				'Content-Type'   : 'application/octet-stream',
+				'Dropbox-API-Arg': '{"path": "{{path}}","mode": "add","autorename": true,"mute": false}'
+			}
+		}
 	},
 
-	responseKeys:
-	{
+	responseKeys: {
 		display_name: 'name.display_name',
-		contents: 'entries',
-		item: 'name',
-		folder: '.tag',
-        id: 'path_display'
+		contents    : 'entries',
+		item        : 'name',
+		folder      : '.tag',
+		id          : 'path_display'
 	},
 
 	rootPath: '/',
